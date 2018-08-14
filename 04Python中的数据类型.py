565273abaae4986ffebffdc885c84b91122d45b3
HowardLei <410905481@qq.com>
2018-08-14 13:38:12 +0800

    new

--- 04Python中的数据类型.py
+++ 04Python中的数据类型.py
@@ -4,6 +4,11 @@ a = 10
 b = 5.2
 #3、创建一个字符串
 c = "123.22"
+#4、创建一个布尔类型的值，True/False 注意：True 为 1，False 为 0
+d = True
+e = False
+string = r"c:\ProgramFiles\new"
+print(string)
 '''
 类型转化函数
 1、int()，即将其他数据类型的数据转化成 int 类型。如果这些数据不全是数，就会报错。
@@ -16,3 +21,9 @@ tempStr = str(a)
 print(tempInt)
 print(tempFloat)
 print(tempStr)
+'''
+判断变量是属于那种数据类型的，用 type() 函数。判断这个数据是不是哪种数据类型，用 isinstance() 
+函数来进行判断。注意；isinstance() 函数中有两个参数，第一个是变量名，第二个是数据类型。返回值为
+bool 类型
+'''
+print(isinstance(tempStr,str))
