00fbd00e6eb1769a0d7002d61b2e064bd1c05f65
HowardLei <410905481@qq.com>
2018-08-18 17:40:31 +0800

    字符串方法总结（未完成）

--- 07列表，元组，字符串.py
+++ 07列表，元组，字符串.py
@@ -2,14 +2,14 @@
 列表：理解起来就是数组的加强版：不受数据类型限制。
 元组：理解起来就是不可变的列表。一旦创建不能修改。
 """
+
+
 # MARK: 列表
-from typing import Any, Union, Tuple
 
 
 def demo1():
     # 1、创建一个列表、注意：这个列表可以为空
-    myList = []
-    actionList = ["eat", "drink"]
+    actionList = ["eat", "dink"]
     # 2、遍历列表 for in 遍历
     for e in actionList:
         print(e)
@@ -29,6 +29,7 @@ def demo1():
     print(actionList2)
     # print(actionList1)
 
+
 # MARK: 元组
 def demo2():
     # 1、创建一个元组。注意：如果创建的是元组，必须含有逗号，如果是单元素元组，则需要在元素后边再
@@ -45,4 +46,31 @@ def demo2():
     print(tuple2)
     del tuple2
 
-demo1()
+
+# MARK: 字符串
+def demo3():
+    name = "王尼玛"
+    # 1、查看字符串中的长度，调用 __len__ 方法。
+    length = name.__len__()
+    print(length)
+    # 2、查找有无字符串及其的位置。调用 find 方法。如果有，则返回第一个字符的索引值，如果没有，
+    #    则返回 -1。
+    # 这个方法还有两个可选参数。一个是 start（起始字符） ，一个是 end（终止字符的位置）
+    hasString = name.find("尼玛")
+    print(hasString)
+    # 3、查找字符串中有没有数字或者是字母，调用 isalnum 方法。如果有，返回 True, 否则返回 False
+    hasAlnum = name.isalnum()
+    print(hasAlnum)
+    # 4、查找字符串中是否只含有字母，调用 isalpha 方法。返回值为 bool 类型。
+    hasAlpha = name.isalpha()
+    print(hasAlpha)
+    # 5、查找字符串中是否只含有数字，调用 isnumeric 方法。
+    hasNum = name.isnumeric()
+    print(hasNum)
+    # 6、用指定编码格式对字符串进行编码，调用 encode 方法。
+    # encode(encoding='编码格式', errors='错误的处理方法，默认为strict')
+    name.encode(encoding='utf-8', errors='strict')
+    # 7、将字符串中 \t 转换为空格。调用 expandtabs()，默认的 tabsize = 8（即空格数转换为 8），
+    newWord = "OK\t"
+
+demo3()
